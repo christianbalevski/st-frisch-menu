ea7340ede56861686b32c05f685895a2557598ba
Christian Balevski <christian.balevski@gmail.com>
2023-05-03 15:41:55 +0000

    changed text

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from streamlit.components.v1 import html
 import pandas as pd
 import csv
-st.set_page_config(page_title="Brainstorming Buddy")
+st.set_page_config(page_title="Frisch Menu Meal Maker")
 
 
 html_temp = """
@@ -13,28 +13,22 @@ html_temp = """
                 </div>
                 """
 
-button = """
-<script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="nainiayoub" data-color="#FFDD00" data-emoji=""  data-font="Cookie" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#ffffff" ></script>
-"""
+# button = """
+# <script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="nainiayoub" data-color="#FFDD00" data-emoji=""  data-font="Cookie" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#ffffff" ></script>
+# """
 
 
 with st.sidebar:
     st.markdown("""
     # About 
-    Brainstorming Buddy is a helper tool built on GPT-3 to generate ideas on a given topic. 
+    Frisch Menu Meal Maker is a helper tool built on GPT-3 to give you menu suggestions based on your needs. 
     """)
     st.markdown(html_temp.format("rgba(55, 53, 47, 0.16)"),unsafe_allow_html=True)
     st.markdown("""
     # How does it work
-    Simply enter the topic of interest in the text field below and ideas will be generated.
-    You can also download the output as txt.
+    Choose your dietary and monetary restrictions and we'll give you some meal suggestions
     """)
     st.markdown(html_temp.format("rgba(55, 53, 47, 0.16)"),unsafe_allow_html=True)
-    st.markdown("""
-    Made by [@nainia_ayoub](https://twitter.com/nainia_ayoub)
-    """,
-    unsafe_allow_html=True,
-    )
 
 
 input_text = None
